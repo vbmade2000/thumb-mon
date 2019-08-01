668b1dae3793b6b34f73469be4e2a7f56407dc25
Malhar Vora <mlvora.2010@gmail.com>
2019-08-01 22:01:24 +0530

    Add configuration reader with configuration file
    This commit adds functionality to read configuration file.

---
+++ src/utils/config_reader.py
@@ -0,0 +1,48 @@
+"""Configuration file reader
+"""
+
+import ConfigParser
+
+
+class ConfigReader(object):
+    """Configuration reader
+    """
+
+    def __init__(self, config_filename):
+        """Initialize ConfigReader
+        """
+        self._config_parser = ConfigParser.RawConfigParser()
+        self._config_parser.read(config_filename)
+
+    def get_value(self, section, key):
+        """Read a single value of a <key> from a <section>.
+
+        Args:
+            section (str): Section of the configuration file to read from.
+            key (str): Key who value you want to read.
+
+        Returns:
+            str: Value
+        """
+        value = self._config_parser.get(section, key)
+        if value == [""]:
+            value = ""
+        return value
+
+    def get_value_list(self, section, key):
+        """Read a list of values of a <key> from a <section>.
+
+        Args:
+            section (str): Section of the configuration file to read from.
+            key (str): Key who value you want to read.
+
+        Returns:
+            list: List of values.
+        """
+        values = []
+        value = self.get_value(section, key)
+        if value != "":
+            raw_values = value.split(",")
+            values = [val.strip() for val in raw_values]
+        return values
+
