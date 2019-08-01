48a10a1d0e6481e62cf24c55352d09fddaea0a81
Malhar Vora <mlvora.2010@gmail.com>
2019-08-01 23:30:09 +0530

    Pass config object in Plugin1

--- src/plugins/plugin1.py
+++ src/plugins/plugin1.py
@@ -7,10 +7,11 @@ from src.plugins.base_plugin import AbstractNotifer
 class Plugin1(AbstractNotifer):
     """Sample plugin class that prints notification to stdout"""
 
-    def __init__(self):
+    def __init__(self, config):
         """Initializes class"""
-        super(Plugin1, self).__init__()
+        super(Plugin1, self).__init__(config)
 
     def send(self):
         """Prints notification to stdout"""
-        print "Plugin1: Thumb drive detected"
+        setting = self._config.get_value("PLUGIN1", "sample_setting")
+        print "Plugin1: Thumb drive detected {0}".format(setting)
