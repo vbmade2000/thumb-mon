8c4dc70027501c45db252db61743a122dd56fc39
Malhar Vora <mlvora.2010@gmail.com>
2019-08-01 23:29:14 +0530

    Add TODOs in logger

--- src/logger.py
+++ src/logger.py
@@ -1,19 +1,26 @@
+"""Contains a necessary functionality to implement logging mechanism
+"""
 import logging
 import logging.handlers
 
 
+# TODO: Make multiple logger and instantiate based on configuration
+# TODO: Add format string to logger
+# TODO: Make log-level configurable
+
+
 class Logger(object):
-    '''Provide functionality to write logs.
+    """Provide functionality to write logs.
        No support for formatters added yet.
-    '''
+    """
 
     def __init__(self, logger_name, log_level=logging.INFO, handlers=None):
-        '''Initialize class'''
+        """Initialize class"""
         self._logger = logging.getLogger(logger_name)
         self._logger.setLevel(log_level)
 
         # Handler to log message to syslog
-        handler = logging.handlers.SysLogHandler(address = '/dev/log')
+        handler = logging.handlers.SysLogHandler(address='/dev/log')
         self._logger.addHandler(handler)
 
         # Add handlers if any
