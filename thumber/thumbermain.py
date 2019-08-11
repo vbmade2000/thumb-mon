e4eda322a5fb4c0849441c022a573a5118fe868f
Malhar Vora <mlvora.2010@gmail.com>
2019-08-11 09:49:38 +0530

    Separate thumber daemon from core logic
    As a good practice we need to create a separate python script
    which should just import core logic and shouldn't contain any
    of it. This file will serve as a main daemon and will be controlled
    by systemd.

--- thumber/thumbermain.py
+++ thumber/thumbermain.py
@@ -127,35 +127,6 @@ def signal_handler(signal, _):
     exit(0)
 
 
-def main(cfg_reader):
-    """Entry point"""
-    # TODO: Put more debug statements
-    # TODO: Send logs with print statement in exception handling
-    # signal.signal(signal.SIGTERM, signal_handler)
-    try:
-
-
-        # TODO: Make logging level configurable
-        # TODO: Make logging available to stdout in case someone runs app
-        #       directly instead of a systemd service
-
-        # Create notifier
-        event_notifier = notifier.Notifier(cfg_reader, logger=thumb_logger)
-        thumb_drive_detector = ThumbDriveDetector(thumb_logger, event_notifier)
-        thumb_drive_detector.detect()
-        thumb_logger.info("Started thumber")
-
-    except KeyboardInterrupt as _:
-        # NOTE: \r can be used to remove ^C characters when printed to STDOUT.
-        #       In syslog it prints like "[32B blob data]".
-        thumb_logger.info("User requested exit. Exiting...")
-    except config_reader.ConfigParser.NoSectionError as no_section_error:
-        thumb_logger.error(
-            "Config: section not found {0}".format(no_section_error.section))
-    except Exception as exception:
-        thumb_logger.error("Error: {0}".format(exception))
-
-
 def get_config_reader():
     # Create config reader
     # TODO: Get conf filepath from command line args. If it is supplied
@@ -171,13 +142,37 @@ def get_config_reader():
     return cfg_reader
 
 
-if __name__ == "__main__":
+def main():
+    """Entry point"""
+    # TODO: Put more debug statements
+    # TODO: Send logs with print statement in exception handling
+    # signal.signal(signal.SIGTERM, signal_handler)
+
     # Application demonizing process closes all the open file descriptors.
     # Here we have config file descriptor. So we moved out config reading
     # logic as we needed a config file descriptor to be passed in
     # DaemonContext context manager to prevent it from closing.
-    config_reader = get_config_reader()
+    cfg_reader = get_config_reader()
+
     with daemon.DaemonContext(
-            files_preserve=[config_reader.get_config_file_descriptor()],
+            files_preserve=[cfg_reader.get_config_file_descriptor()],
             signal_map={signal.SIGTERM: signal_handler}):
-        main(config_reader)
+
+        try:
+            # TODO: Make log level configurable
+            # Create notifier
+            event_notifier = notifier.Notifier(cfg_reader, logger=thumb_logger)
+            thumb_drive_detector = ThumbDriveDetector(thumb_logger, event_notifier)
+            thumb_drive_detector.detect()
+            thumb_logger.info("Started thumber")
+
+        except KeyboardInterrupt as _:
+            # NOTE: \r can be used to remove ^C characters when printed to STDOUT.
+            #       In syslog it prints like "[32B blob data]".
+            thumb_logger.info("User requested exit. Exiting...")
+        except config_reader.ConfigParser.NoSectionError as no_section_error:
+            thumb_logger.error(
+                "Config: section not found {0}".format(no_section_error.section))
+        except Exception as exception:
+            thumb_logger.error("Error: {0}".format(exception))
+
